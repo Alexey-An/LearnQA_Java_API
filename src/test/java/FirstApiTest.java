ae5b7995d91beba9e061a85a48feb0254979c055
Alexey Anosov <a.anosov15@yandex.ru>
2023-12-01 09:45:49 +0500

    test(Learn_QA_Java_API): добавлен первый тест

---
+++ src/test/java/FirstApiTest.java
@@ -0,0 +1,11 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class FirstApiTest {
+    @Test
+    public void testGetText() {
+        Response response = RestAssured.get("https://playground.learnqa.ru/api/get_text");
+        response.prettyPrint();
+    }
+}
