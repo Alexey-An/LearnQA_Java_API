f088bfa5a0ee2a5facc4445044873e63b4587e5f
Alexey Anosov <a.anosov15@yandex.ru>
2023-12-11 17:29:01 +0500

    test(Learn_QA_Java_API): добавелен testLongRedirect для проверки редиректа

--- src/test/java/FirstApiTest.java
+++ src/test/java/FirstApiTest.java
@@ -1,4 +1,5 @@
 import io.restassured.RestAssured;
+import io.restassured.http.Header;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
@@ -14,5 +15,22 @@ public class FirstApiTest {
     public void testGetJson() {
         JsonPath response = RestAssured.get("https://playground.learnqa.ru/api/get_json_homework").jsonPath();
         System.out.println(response.get("messages[1].message").toString());
+    }
+
+    @Test
+    public void testLongRedirect() {
+        Response response = RestAssured.given()
+                .redirects()
+                .follow(false)
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+        System.out.println(response.getStatusCode());
+        Header location = response.getHeaders()
+                .asList()
+                .stream()
+                .filter(header -> "location".equalsIgnoreCase(header.getName()))
+                .findAny()
+                .orElseThrow(() -> new RuntimeException("заголовок location не найден!"));
+        System.out.println(String.format( "%s: %s", location.getName(), location.getValue() ));
     }
 }
