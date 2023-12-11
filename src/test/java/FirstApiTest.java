b8969be5b4e2a428193c8246428b68071fceafdb
Alexey Anosov <a.anosov15@yandex.ru>
2023-12-11 14:08:48 +0500

    test(Learn_QA_Java_API): добавелен testGetJson для проверки JSON, который отдает сервер при отправке get-запроса на эндпоинт /api/get_json_homework

--- src/test/java/FirstApiTest.java
+++ src/test/java/FirstApiTest.java
@@ -1,4 +1,5 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
@@ -7,5 +8,11 @@ public class FirstApiTest {
     public void testGetText() {
         Response response = RestAssured.get("https://playground.learnqa.ru/api/get_text");
         response.prettyPrint();
+    }
+
+    @Test
+    public void testGetJson() {
+        JsonPath response = RestAssured.get("https://playground.learnqa.ru/api/get_json_homework").jsonPath();
+        System.out.println(response.get("messages[1].message").toString());
     }
 }
