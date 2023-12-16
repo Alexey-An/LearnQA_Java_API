e1b6ffb0cdb8ded3f08eec981589395c48068d56
Alexey Anosov <a.anosov15@yandex.ru>
2023-12-17 03:38:41 +0500

    test(Learn_QA_Java_API): добавлен тест для проверки длины случайной строки (больше 15 символов или нет)

---
+++ src/test/java/Week3Tests.java
@@ -0,0 +1,30 @@
+import io.restassured.RestAssured;
+import io.restassured.http.Header;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class Week3Tests {
+    @ParameterizedTest
+    @ValueSource(strings = {"Short string", "Random very long string"})
+    public void testStringLength(String testString) {
+        assertTrue(testString.length() > 15);
+    }
+
+}
